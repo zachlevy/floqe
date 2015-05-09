ad6245faec2aad12f06f93779e3d938e73722f74
Zachary Levy <zacharyalevy@gmail.com>
2015-05-08 21:28:38 -0400

    remove starter app views and controllers

--- www/js/app.js
+++ www/js/app.js
@@ -87,43 +87,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
   })
   // FLOQUE END
 
-  .state('app.search', {
-    url: "/search",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/search.html"
-      }
-    }
-  })
-
-  .state('app.browse', {
-    url: "/browse",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/browse.html"
-      }
-    }
-  })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent': {
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-  .state('app.single', {
-    url: "/playlists/:playlistId",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/playlist.html",
-        controller: 'PlaylistCtrl'
-      }
-    }
-  })
-
   ; // end chaining
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/conversations/1');
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -33,22 +33,8 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-})
 
-// Tags Search Screen
-.controller('TagsSearchController', function($scope, $state, tagsFactory, allTags, $timeout, appHelper, post_api_tags_suggested) {
   // reset global allTags value to make sure they're up to date
   tagsFactory.refreshTags();
 
