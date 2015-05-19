781985dc969e186b72117caf19a22e948652743e
Zachary Levy <zacharyalevy@gmail.com>
2015-05-19 17:48:58 -0400

    merging to master

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -45,4 +45,163 @@ angular.module('starter.controllers', [])
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+})
+
+// Tags Search Screen
+.controller('TagsSearchController', function($scope, $state, tagsFactory, allTags) {
+  // reset global allTags value to make sure they're up to date
+  tagsFactory.refreshTags();
+
+  // default tags
+  searchTags = [];
+  updateSearch();
+  // get all tags
+  $scope.suggestedTags = tagsFactory.suggested();
+  $scope.allTags = tagsFactory.all();
+
+  // add a tag
+  $scope.addTag = function(tagId) {
+    var selectedTag = $scope.allTags.filter(function(tag) {
+      return tag.id == tagId;
+    });
+    searchTags.push(selectedTag[0]);
+    updateSearch();
+  };
+
+  // process search
+  $scope.submitTags = function () {
+    console.log("submitTags");
+    console.log(searchTags);
+    searchTagsText = tagsFactory.idsList(searchTags);
+    $state.go('app.tagsResults', {tag_ids: searchTagsText});
+  };
+
+  // output the names of search tags
+  function updateSearch() {
+    console.log("updateSearch");
+    $scope.search = [];
+    angular.forEach(searchTags, function (value, key) {
+      this.push(value.name);
+    }, $scope.search);
+  }
+})
+
+// Tags Search Results Screen
+.controller('TagsResultsController', function($scope, $state, $stateParams, tagsFactory, usersFactory, post_api_search, current_user, allTags, $ionicSideMenuDelegate) {
+  tagsFactory.refreshTags(); // temp
+  console.log('TagsResultsController');
+
+  // init
+  // hit API server for results
+  users = post_api_search['result']['users'];
+  searchedTags = post_api_search['result']['tags'];
+  $scope.me = current_user;
+  $scope.tagNames = tagsFactory.namesList(searchedTags);
+  
+  $scope.users = users;
+
+  // default filters
+  $scope.filters = {};
+  $scope.filters.proximity = 5;
+  $scope.filters.ageMin = 18;
+  $scope.filters.ageMax = 44;
+  $scope.filters.male = true;
+  $scope.filters.female = true;
+
+  // by default, hide the filter list
+  $scope.hideFilter = true;
+
+  // handle add/hide filters button
+  $scope.hideFilters = function () {
+    $scope.hideFilter = true;
+  };
+  $scope.showFilters = function () {
+    $scope.hideFilter = false;
+  };
+
+  // custom filter for age, proximity, gender
+  $scope.filterUsers = function(user){
+    if ($scope.showFilters) {
+      return user.age >= $scope.filters.ageMin &&
+      user.distance <= $scope.filters.proximity &&
+      user.age <= $scope.filters.ageMax &&
+      (
+        (user.gender === 0 && $scope.filters.male) ||
+        (user.gender === 1 && $scope.filters.female)
+      )
+      ;
+    } else {
+      return user;
+    }
+  };
+
+  // swiping
+  $scope.swipedUserIds = [];
+
+  $scope.onSwipeRight = function(user_id) {
+    console.log("Swiped Right");
+    if ($scope.swipedUserIds.indexOf(user_id) == -1) {
+      $scope.swipedUserIds.push(user_id);
+    }
+    // send to API 
+    // implement
+    console.log($scope.swipedUserIds);
+    console.log($stateParams.search_id);
+  };
+
+  $scope.onSwipeLeft = function(user_id) {
+    console.log("Swiped Left");
+    var index = $scope.swipedUserIds.indexOf(user_id);
+    if (index > -1) {
+      $scope.swipedUserIds.splice(index, 1);
+    }
+    // send to API 
+    // implement
+    console.log($scope.swipedUserIds);
+    console.log($stateParams.search_id);
+  };
+})
+
+// Matches Screen
+.controller('MatchesController', function($scope, $state, $stateParams, post_api_match_mine) {
+  console.log('MatchesController');
+  $scope.matches = post_api_match_mine['result'];
+  $scope.onMatchSelect = function(match_id){
+    console.log('match selected');
+    console.log(match_id);
+  };
+})
+
+// Invite Matches Screen
+.controller('MatchesInviteController', function($scope, $state, $stateParams, post_api_match_mine) {
+  console.log('InviteMatchesController');
+  $scope.matches = post_api_match_mine['result'];
+  
+  // for match
+  console.log();
+
+  // swiping
+  $scope.selectedMatchIds = [];
+
+  // when match selected
+  $scope.onMatchSelect = function(match_id){
+    console.log('match selected');
+    if ($scope.selectedMatchIds.indexOf(match_id) == -1) {
+      $scope.selectedMatchIds.push(match_id);
+    } else if ($scope.selectedMatchIds.indexOf(match_id) > -1) {
+      $scope.selectedMatchIds.splice($scope.selectedMatchIds.indexOf(match_id), 1);
+    }
+    console.log($scope.selectedMatchIds);
+  };
+
+  // when invite button clicked
+  $scope.onInvite = function(){
+    // go back to convo screen
+    console.log($stateParams.match_id);
+    console.log($scope.selectedMatchIds);
+    // send to API 
+    // implement
+  };
+})
+
+; // ends chaining
---
+++ www/js/factories.js
@@ -0,0 +1,334 @@
+angular.module('starter.factories', [])
+
+.factory('appApi', function ($http, baseUrl) {
+  // data is an object like {'foo':'bar'}
+  // endpoint is a string
+  return {
+    send: function(endpoint, data) {
+      $http.post(baseUrl, data)
+      .success(function(data, status, headers, config) {
+        if (data.success === true) {
+          return data.result;
+        } else if (data.success === false) {
+          return false;
+        } else {
+          return null;
+        }
+      }).error(function(data, status, headers, config) {
+        console.log(status);
+        return status;
+      });
+    },
+    recieve: function(endpoint) {
+      $http.get(baseUrl + endpoint)
+      .success(function(data, status, headers, config) {
+        return data;
+      }).error(function(data, status, headers, config) {
+        return status;
+      });
+    }
+  };
+})
+
+.factory('usersFactory', function ($http){
+  // factory vars
+
+  // factory functions
+  return {
+    getUser: function (id, users) {
+      var results = users.filter(function(user) {
+        return user.id == id;
+      });
+      return results[0];
+    },
+    // send a swipe to API server
+    sendSwipe: function (user_id) {
+
+    }
+  };
+})
+
+.factory('tagsFactory', function($http, allTags){
+  // factory vars
+  var suggested = function() {
+    return [
+      {
+        "id" : 1,
+        "name" : "Motor rallies"
+      },
+      {
+        "id" : 2,
+        "name" : "Motor"
+      },
+    ];
+  };
+  // factory functions
+  return {
+    refreshTags: function () {
+      allTags.tags = this.all();
+    },
+    // returns tag objects based on ids string like 1,2,3,4
+    // requires its own factory because super this unaccessible
+    tagsFromIdsList: function (tag_ids, tagsFactory) {
+      tags = [];
+      tag_ids.split(',').map(function(tag_id) {
+        tags.push(tagsFactory.getTag(tag_id));
+      });
+      return tags;
+    },
+    // get tag object from tag id
+    getTag: function (id) {
+      var results = allTags.tags.filter(function(tag) {
+        return tag.id == id;
+      });
+      return results[0];
+    },
+    // returns tag ids like 1,2,3,4 from tag objects
+    idsList: function (tags) {
+      text = [];
+      angular.forEach(tags, function (value, key) {
+        this.push(value.id);
+      }, text);
+      return text.join(',');
+    },
+    // returns tag names like basketball,hockey from tag objects
+    namesList: function(tags) {
+      text = [];
+      angular.forEach(tags, function (value, key) {
+      this.push(value.name);
+      }, text);
+      return text.join(', ');
+    },
+    // get suggested tags for tag search screen
+    suggested: suggested,
+    all: function() {
+      return [
+        {
+          "id" : 1,
+          "name" : "Motor rallies"
+        },
+        {
+          "id" : 2,
+          "name" : "Motor"
+        },
+        {
+          "id" : 3,
+          "name" : "Volleyball"
+        },
+        {
+          "id" : 4,
+          "name" : "Horses"
+        },
+        {
+          "id" : 5,
+          "name" : "Dog Walking"
+        }
+      ];
+    }
+  };
+})
+// generic application helper for common js functions
+.factory('appHelper', function(){
+  // factory vars
+
+  // factory functions
+  return {
+    // array add
+    addIfNotExists: function (item, array) {
+      
+    },
+    // array remove
+    removeIfExists: function (item, array) {
+
+    }
+  };
+})
+// API base url
+.value(
+  'baseUrl',
+  'http://backend-env-36mjm8eh3x.elasticbeanstalk.com/api/v1/'
+)
+.value(
+  'allTags',
+  {
+    'tags' : []
+  }
+)
+
+
+
+// development helpers
+// current_user
+.value(
+  'current_user',
+  {
+    "id" : 3,
+    "name" : "Boaz",
+    "photo" : "http://placehold.it/100x100",
+    "description" : null,
+    "gender" : 1,
+    "age" : 23
+  }
+)
+
+// GET /api/tags
+.value(
+  'get_api_tags',
+  [
+    {
+      "id" : 1,
+      "name" : "Motor rallies"
+    },
+    {
+      "id" : 2,
+      "name" : "Motor"
+    },
+    {
+      "id" : 3,
+      "name" : "Volleyball"
+    },
+    {
+      "id" : 4,
+      "name" : "Horses"
+    },
+    {
+      "id" : 5,
+      "name" : "Dog Walking"
+    }
+  ]
+)
+// POST /api/search
+.value(
+  'post_api_search',
+  {
+    "success" : true,
+    "result" : {
+      "id" : 1,
+      "tags" : [
+        {
+          "id" : 1,
+          "name" : "Hockey"
+        },
+        {
+          "id" : 2,
+          "name" : "Basketball"
+        }
+      ],
+      "users" : [
+        {
+          "id" : 1,
+          "name" : "Jake",
+          "description" : "pickup",
+          "photo" : "http://placehold.it/100x100",
+          "friend" : true,
+          "gender" : 0,
+          "distance" : 2.1,
+          "age" : 26,
+          "matched" : true
+        },
+        {
+          "id" : 2,
+          "name" : "Alex",
+          "description" : "Scrim",
+          "photo" : "http://placehold.it/100x100",
+          "friend" : false,
+          "gender" : 1,
+          "distance" : 5,
+          "age" : 23,
+          "matched" : false
+        }
+      ]
+    }
+  }
+)
+// POST /api/search/description
+.value(
+  'post_api_search_description',
+  {
+    "success" : true
+  }
+)
+// POST /api/match/me
+.value(
+  'post_api_match_me',
+  {
+    "success" : true
+  }
+)
+// POST /api/match/mine
+.value(
+  'post_api_match_mine',
+  {
+    "success" : true,
+    "result" : [
+      {
+        "id": 1,
+        "last_message" : {
+          "user_id" : 1,
+          "message" : "Cya there bro",
+          "time" : "2015-04-26T00:00:00Z",
+          "unread" : true
+        },
+        "users" : [
+          {
+            "id" : 1,
+            "name" : "Jake",
+            "photo" : "http://placehold.it/100x100",
+            "friend" : true,
+          }
+        ],
+        "matched_for" : [
+          {
+            "id" : 1,
+            "name" : "Hockey",
+            "time" : "2015-04-22T01:11:21Z"
+           }
+         ],
+      },
+      {
+        "id": 2,
+        "last_message" : null,
+        "users" : [
+          {
+            "id" : 1,
+            "name" : "Jake",
+            "photo" : "http://placehold.it/100x100",
+            "friend" : true,
+          },
+          {
+            "id" : 2,
+            "name" : "Alex",
+            "description" : "Scrim",
+            "photo" : "http://placehold.it/100x100",
+            "friend" : false,
+          }
+        ],
+        "matched_for" : {
+          "time" : "2015-04-25T02:15:31Z",
+          "tags" : [
+            {
+              "id" : 1,
+              "name" : "Hockey",
+            }
+          ]
+        }
+      }
+    ]
+  }
+)
+// POST /api/invite
+.value(
+  'post_api_invite',
+  {
+    "success": true
+  }
+)
+// POST /api/message/read
+.value(
+  'post_api_message_read',
+  {
+    "success" : true
+  }
+)
+
+; // end chaining
